858b6a8c58ef1111557292b773a15f603fe209fa
aphilippe <aphilippe@iMac-de-aphilippe.local>
2015-11-20 14:33:30 +0100

    Use platform parameter for projectCreator script

--- projectCreator.sh
+++ projectCreator.sh
@@ -4,11 +4,21 @@
 
 #TODO
 #	platform as a parameter
+#		Verify if platform exists
 #	param clean platform -> delete "platform" folder
 #	param clean all -> delete "projects" folder
 
 projectFolder=projects
-platform=Xcode
+
+# We need at least one parameter
+if [ -z $1 ]
+then
+	echo "How to use $0 :"
+	echo "$0 <name of plateform> : create projet for the platform"
+	exit
+fi
+
+platform=$1
 
 # First create folder for all builds
 mkdir -p $projectFolder/$platform
