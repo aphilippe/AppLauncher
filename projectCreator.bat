1497768382b623c6b147ea4a126dd0b820e342d8
Adrien Philippe <Adrien Philippe>
2015-11-21 19:22:53 +0100

    Create project for visual studio 14

--- projectCreator.bat
+++ projectCreator.bat
@@ -1,8 +1,18 @@
-echo off
+rem echo off
 
+SET mypath=%~dp0
 set platformsFolder=platforms
-set platformName=Visual
+set platformName="Visual Studio 14"
 
-set folder=%platformsFolder%\%platformName%
+set folder=%mypath%\%platformsFolder%\%platformName%
 
 if not exist %folder% (mkdir %folder%)
+
+cd %platformsFolder%
+
+rem call cmake
+cmake.exe .. -G%platformName%
+
+cd %mypath%
+
+pause
