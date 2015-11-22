43495a899c1ac1c4d034167d6c99dc656c7065eb
Adrien Philippe <Adrien Philippe>
2015-11-22 13:42:26 +0100

    add clean and cleanAll parameters

--- projectCreator.bat
+++ projectCreator.bat
@@ -1,8 +1,28 @@
-echo off
+@echo off
 
+SET cleanParameter=clean
+SET cleanAllParamter=cleanAll
+
+SET firstParameter=%1
+if %firstParameter% EQU %cleanAllParamter% (
+	echo "cleanAll not implemented"
+) else if %firstParameter% EQU %cleanParameter% (
+	echo "clean not implemented"
+) else (
+	goto:createProject
+)
+
+pause
+goto:eof
+
+::--------------------------------------------------------
+::-- Function section starts below here
+::--------------------------------------------------------
+
+:createProject
 SET mypath=%~dp0
 set platformsFolder=platforms
-set platformName=%1
+set platformName=%*
 
 set folder=%mypath%\%platformsFolder%\%platformName%
 
@@ -14,5 +34,4 @@ rem call cmake
 cmake.exe %mypath% -G%platformName%
 
 cd %mypath%
-
-pause
+goto:eof
