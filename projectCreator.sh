8e598830a2b45e2588d8822a3b37157a80b155e3
aphilippe <aphilippe@iMac-de-aphilippe.local>
2015-11-27 12:52:52 +0100

    create function for check params and create_project + prepare clean and cleanAll

--- projectCreator.sh
+++ projectCreator.sh
@@ -10,21 +10,44 @@
 
 projectFolder=projects
 
-# We need at least one parameter
-if [ -z $1 ]
+check_params () {
+	# We need at least one parameter
+	if [ -z $1 ]
+	then
+		echo >&2 "How to use $0 :"
+		echo >&2 "$0 <name of plateform> : create projet for the platform"
+		return 0
+	elif [ $1 == cleanAll ]
+	then
+		return 1
+	elif [ $1 == clean ]
+	then
+		return 2
+	else
+		return 3
+	fi
+}
+
+create_project () {
+	platform=$1
+	# First create folder for all builds
+	mkdir -p $projectFolder/$platform
+	# change directory
+	cd $projectFolder/$platform
+	# execute cmake command
+	cmake ../../. -G"$platform"
+}
+
+check_params $@
+retval=$?
+if [ $retval == 1 ]
 then
-	echo "How to use $0 :"
-	echo "$0 <name of plateform> : create projet for the platform"
-	exit
+	echo "cleanAll"
+elif [ $retval == 2 ]
+then
+	echo "clean"
+elif [ $retval == 3 ]
+then
+	create_project $@
 fi
 
-platform=$1
-
-# First create folder for all builds
-mkdir -p $projectFolder/$platform
-
-# change directory
-cd $projectFolder/$platform
-
-# execute cmake command
-cmake ../../. -G"$platform"
