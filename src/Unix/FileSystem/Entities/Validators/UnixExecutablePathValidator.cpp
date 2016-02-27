4011c7d82e0f83caa484d9610235cdcdd7df7b7c
Adrien Philippe <Adrien Philippe>
2016-02-27 16:35:59 +0100

    Add exception management on Unix

--- src/Unix/FileSystem/Entities/Validators/UnixExecutablePathValidator.cpp
+++ src/Unix/FileSystem/Entities/Validators/UnixExecutablePathValidator.cpp
@@ -5,8 +5,20 @@ using namespace clt::filesystem::entities::validators;
 
 bool
 UnixExecutablePathValidator::isPathValid(const Path& path, std::vector<ValidatorBrokenRules> & brokenRules) const {
-//TODO: must be a file with executable right
-	return path.exists()
-		&& !path.isDirectory()
-		&& path.isExecutable();
+	if (!path.exists()) {
+		brokenRules.push_back(ValidatorBrokenRules("Path does not exist"));
+		return false;
+	}
+
+	if (path.isDirectory()) {
+		brokenRules.push_back(ValidatorBrokenRules("The path is not a file"));
+		return false;
+	}
+
+	if (!path.isExecutable()) {
+		brokenRules.push_back(ValidatorBrokenRules("File at path is not executable"));
+		return false;
+	}
+
+	return true;
 }
